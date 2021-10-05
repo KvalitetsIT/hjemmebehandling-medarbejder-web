abf3a20d3adb720c9c0ba874fee2131e78d4294d
nilsj <njo@kvalitetsit.dk>
2021-10-05 16:27:58 +0200

    Delete java-stuff.

--- documentation/docker/getOldDoc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if docker pull kvalitetsit/rim-medarbejder-web-documentation:latest; then
-    echo "Copy from old documentation image."
-    docker cp $(docker create kvalitetsit/rim-medarbejder-web-documentation:latest):/usr/share/nginx/html target/old
-fi
