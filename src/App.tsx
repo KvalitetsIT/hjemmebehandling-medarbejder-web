a7920fe09ec9fd6b7c72b259a14a288949adefdb
nilsj <njo@kvalitetsit.dk>
2021-10-07 14:55:41 +0200

    Extend to call medarbejder-bff.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ function foo(x: number) {
   return x;
 }
 
+function bar() {
+  console.log('calling bff ...');
+
+  fetch('http://localhost:8080/api/v1/hello', { 'method': 'POST', 'body': '{"name": "Slartibartfast"}', 'headers': new Headers({'Content-Type': 'application/json'})})
+  .then(res => res.json())
+  .then(res => console.log(res));
+}
+
 function App() {
-  console.log('Computed foo: ' + foo(4));
+  console.log('Calling foo. Result: ' + foo(4));
 
   return (
     <div className="App">
@@ -24,6 +32,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={bar}>Call medarbejder-bff</button>
       </header>
     </div>
   );
