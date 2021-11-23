9ac6ffd43381a7ce2a0f926fa6079c8d370b78eb
Mikkel Birch <mib@kvalitetsit.dk>
2021-11-23 14:09:02 +0100

    Cpr validation

--- services/ValidationService.tsx
+++ services/ValidationService.tsx
@@ -38,6 +38,11 @@ export default class ValidationService extends BaseService implements IValidatio
             const error = new InvalidInputModel(propName,"skal være 10 tegn")
             erorrs.push(error)
         }
+        if(cpr?.length == 10 && !this.CalculateCPR(cpr)){
+            const error = new InvalidInputModel(propName,"Ikke korrekt CPR")
+            erorrs.push(error)
+        }
+
         return erorrs;
     }
 
@@ -70,6 +75,28 @@ export default class ValidationService extends BaseService implements IValidatio
         }
 
         return errors
+    }
+
+
+    private CalculateCPR(cpr : string) : boolean{
+        //http://kode.tingeling.dk/cpr_fix/
+        if(cpr.length != 10)
+            return false;
+            
+        const cprNumbers = cpr.split("").map(x=>x as unknown as number);
+        const controlNumber = [4,3,2,7,6,5,4,3,2]
+        const constant = 11;
+
+        let sum = 0;
+        for(let i = 0; i<9; i++){
+            let cprDigit = cprNumbers[i];
+            let controlDigit = controlNumber[i];
+            sum += cprDigit * controlDigit;
+        }
+
+        let b1 = constant - (sum % constant)
+        let isCorrectCpr = b1 == cprNumbers[9]
+        return isCorrectCpr
     }
 
 
