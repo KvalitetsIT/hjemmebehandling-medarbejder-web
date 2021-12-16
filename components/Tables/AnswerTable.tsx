5a2a83d726f8791454b60fbb86a6c822ecdd00af
Mikkel Birch <mib@kvalitetsit.dk>
2021-12-16 13:03:39 +0100

    No threshold matching answer => Make cell blue

--- components/Tables/AnswerTable.tsx
+++ components/Tables/AnswerTable.tsx
@@ -16,201 +16,203 @@ import { LoadingSmallComponent } from '../Layout/LoadingSmallComponent';
 import { ErrorBoundary } from '../Layout/ErrorBoundary';
 
 export interface Props {
-    questionnaires : Questionnaire
-    careplan : PatientCareplan
+    questionnaires: Questionnaire
+    careplan: PatientCareplan
 }
 
 export interface State {
-  thresholdModalOpen : boolean
-  questionnaireResponses : QuestionnaireResponse[]
-loading: boolean
-  pagesize : number
-    page : number
+    thresholdModalOpen: boolean
+    questionnaireResponses: QuestionnaireResponse[]
+    loading: boolean
+    pagesize: number
+    page: number
 }
 
-export class AnswerTable extends Component<Props,State> {
-  static displayName = AnswerTable.name;
-  static contextType = ApiContext
+export class AnswerTable extends Component<Props, State> {
+    static displayName = AnswerTable.name;
+    static contextType = ApiContext
 
-  questionAnswerService! : IQuestionAnswerService;
-  questionnaireService! : IQuestionnaireService;
-  datehelper! : IDateHelper
+    questionAnswerService!: IQuestionAnswerService;
+    questionnaireService!: IQuestionnaireService;
+    datehelper!: IDateHelper
 
-constructor(props : Props){
-    super(props);
-    this.state = {
-        thresholdModalOpen : false,
-        questionnaireResponses : [],
-        loading: true,
-        pagesize : 6,
-        page : 1
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            thresholdModalOpen: false,
+            questionnaireResponses: [],
+            loading: true,
+            pagesize: 6,
+            page: 1
+        }
     }
-}
 
-  render () : JSX.Element{
-    this.InitializeServices();
-    const contents = this.renderTableData(this.props.questionnaires);
-    return contents;
-  }
+    render(): JSX.Element {
+        this.InitializeServices();
+        const contents = this.renderTableData(this.props.questionnaires);
+        return contents;
+    }
 
-  InitializeServices() : void{
-    this.questionAnswerService = this.context.questionAnswerService;
-    this.questionnaireService = this.context.questionnaireService;
-    this.datehelper = this.context.dateHelper;
-  }
+    InitializeServices(): void {
+        this.questionAnswerService = this.context.questionAnswerService;
+        this.questionnaireService = this.context.questionnaireService;
+        this.datehelper = this.context.dateHelper;
+    }
 
-  async componentDidMount() :  Promise<void>{
-      try{
-      await this.populateData(this.state.page)
-    }  catch(error : any){
-        this.setState(()=>{throw error})
-      }  
-  }
-    async populateData(page : number) :  Promise<void> {
-        this.setState({loading: true})
+    async componentDidMount(): Promise<void> {
+        try {
+            await this.populateData(this.state.page)
+        } catch (error: any) {
+            this.setState(() => { throw error })
+        }
+    }
+    async populateData(page: number): Promise<void> {
+        this.setState({ loading: true })
         const careplan = this.props.careplan
-        const questionnaireResponses = await this.questionnaireService.GetQuestionnaireResponses(careplan.id,[this.props.questionnaires.id],page,this.state.pagesize)
-        this.setState({questionnaireResponses : []}) //Without this the StatusSelect will not destroy and recreate status-component, which will result it to show wrong status (JIRA: RIM-103)
-        this.setState({questionnaireResponses : questionnaireResponses,page : page, loading: false})
+        const questionnaireResponses = await this.questionnaireService.GetQuestionnaireResponses(careplan.id, [this.props.questionnaires.id], page, this.state.pagesize)
+        this.setState({ questionnaireResponses: [] }) //Without this the StatusSelect will not destroy and recreate status-component, which will result it to show wrong status (JIRA: RIM-103)
+        this.setState({ questionnaireResponses: questionnaireResponses, page: page, loading: false })
 
     }
 
- async NextPage() :  Promise<void>{
-    const currenpage = this.state.page;
-    const nextPage =currenpage+1
-    await this.populateData(nextPage)
+    async NextPage(): Promise<void> {
+        const currenpage = this.state.page;
+        const nextPage = currenpage + 1
+        await this.populateData(nextPage)
 
-    this.forceUpdate()
-}
-
- async PreviousPage() : Promise<void> {
-     const currenpage = this.state.page;
-     const nextPage = currenpage-1
-     await this.populateData(nextPage)
-
-    this.forceUpdate()
-}
-
-getChipColorFromCategory(category : CategoryEnum) : "error" | "warning"|"primary" | "default" | "success"{
-    if(category === CategoryEnum.RED)
-        return "error"
-    if(category === CategoryEnum.YELLOW)
-        return "warning"
-    if(category === CategoryEnum.GREEN)
-        return "success"
-    if(category === CategoryEnum.BLUE)
-        return "primary"
-
-    return "default"
-}
-
-statusUpdate(status : QuestionnaireResponseStatus, questionnaireResponse : QuestionnaireResponse) : void {
-    questionnaireResponse.status = status;
-    this.forceUpdate();
-
-}
-
-  renderTableData(questionaire : Questionnaire) : JSX.Element{
-    if(this.state.loading)
-        return (<LoadingSmallComponent/>)
-
-    const questionaireResponses = this.state.questionnaireResponses;
-    if(!questionaireResponses || questionaireResponses.length === 0 && this.state.page == 1){
-        return (
-            <>
-            <Typography>Ingen besvarelser for spørgeskema endnu. </Typography>
-            <Typography variant="caption">Spørgeskemaet besvares {questionaire.frequency.ToString()}</Typography>
-            </>
-        )
+        this.forceUpdate()
     }
 
-    
-    let hasMorePages = false;
+    async PreviousPage(): Promise<void> {
+        const currenpage = this.state.page;
+        const nextPage = currenpage - 1
+        await this.populateData(nextPage)
 
-    
+        this.forceUpdate()
+    }
 
-    if(this.state.questionnaireResponses.length === this.state.pagesize)
-        hasMorePages = true;
+    getChipColorFromCategory(category: CategoryEnum): "error" | "warning" | "primary" | "default" | "success" {
+        if (category === CategoryEnum.RED)
+            return "error"
+        if (category === CategoryEnum.YELLOW)
+            return "warning"
+        if (category === CategoryEnum.GREEN)
+            return "success"
+        if (category === CategoryEnum.BLUE)
+            return "primary"
 
-    const questionnairesResponsesToShow = this.state.questionnaireResponses.slice(0,this.state.pagesize)
- 
-    
-    
-    return (<>
-    <Box className="answer__pagination-buttons" textAlign="right">
-    <Button disabled={!hasMorePages} onClick={async ()=>await this.NextPage()} startIcon={<NavigateBeforeIcon/>}>Nyere</Button>
-    <Button disabled={this.state.page <= 1} onClick={async ()=>await this.PreviousPage()} endIcon={<NavigateNextIcon  />}>Ældre</Button>
-              
+        return "default"
+    }
+
+    statusUpdate(status: QuestionnaireResponseStatus, questionnaireResponse: QuestionnaireResponse): void {
+        questionnaireResponse.status = status;
+        this.forceUpdate();
+
+    }
+
+    renderTableData(questionaire: Questionnaire): JSX.Element {
+        if (this.state.loading)
+            return (<LoadingSmallComponent />)
+
+        const questionaireResponses = this.state.questionnaireResponses;
+        if (!questionaireResponses || questionaireResponses.length === 0 && this.state.page == 1) {
+            return (
+                <>
+                    <Typography>Ingen besvarelser for spørgeskema endnu. </Typography>
+                    <Typography variant="caption">Spørgeskemaet besvares {questionaire.frequency.ToString()}</Typography>
+                </>
+            )
+        }
+
+
+        let hasMorePages = false;
+
+
+
+        if (this.state.questionnaireResponses.length === this.state.pagesize)
+            hasMorePages = true;
+
+        const questionnairesResponsesToShow = this.state.questionnaireResponses.slice(0, this.state.pagesize)
+
+
+
+        return (<>
+            <Box className="answer__pagination-buttons" textAlign="right">
+                <Button disabled={!hasMorePages} onClick={async () => await this.NextPage()} startIcon={<NavigateBeforeIcon />}>Nyere</Button>
+                <Button disabled={this.state.page <= 1} onClick={async () => await this.PreviousPage()} endIcon={<NavigateNextIcon />}>Ældre</Button>
+
             </Box>
-    <TableContainer component={Paper}>
-    
-      <Table aria-label="simple table">
-        <TableHead>
-            
-          <TableRow className='table__row'>
-          <TableCell>
-         
-            </TableCell>
-            {questionnairesResponsesToShow.map(collection => {
+            <TableContainer component={Paper}>
 
-                let severity = this.getChipColorFromCategory(collection.category) as string
-               
-                if(collection.status === QuestionnaireResponseStatus.Processed)
-                    severity = "info"
-                    
-                return (
-                    <TableCell className="answer__table-head" align="center">
-     
-                            <Stack className='answer__header-color' component={Alert} spacing={1} alignItems="center" alignContent="center" alignSelf="center" textAlign="center" icon={false} severity={severity as AlertColor}>
-                                <div className="answer__header">
-                                    <Typography className="answer__headline" align="center">{collection.answeredTime ? this.datehelper.DayIndexToDay(collection.answeredTime.getUTCDay()) : ""}</Typography>
-                                    <Typography className="answer__date" align="center" variant="caption">{collection.answeredTime ? this.datehelper.DateToString(collection.answeredTime) : ""}</Typography>
-                                </div>
-                                
-                                <ErrorBoundary rerenderChildren={false}>
-                                    <QuestionnaireResponseStatusSelect onUpdate={status => this.statusUpdate(status, collection)} questionnaireResponse={collection} />  
-                                </ErrorBoundary>
-                            </Stack>
-                     
-                      
-       
-                      </TableCell>
-                    
-                )
-            })}
-            
-          </TableRow>
-            
-        </TableHead>
-        <TableBody>
-                    {this.questionnaireService.findAllQuestions(questionnairesResponsesToShow).map(question => {
-                        return (
-                            <>
-                            
-                            <TableRow>
-                                <TableCell>
-                                    {question.question}                                    
-                                </TableCell>
-                                
-                                {questionnairesResponsesToShow.map(questionResponse => {
-                                    const answer = this.questionnaireService.findAnswer(question,questionResponse);
-                                    const thresholdCollection = this.props.questionnaires.thresholds.find(x=>x.questionId == question.Id);
-                                    const category = answer && thresholdCollection ? this.questionAnswerService.FindCategory(thresholdCollection,answer) : CategoryEnum.GREEN;
-                                    return (
-                                        <TableCell> <Chip className='answer__chip' component={Box} width="100%" size="medium"  color={this.getChipColorFromCategory(category)} label={answer ? answer.ToString() : ""} variant="filled" /></TableCell>
-                                    )
-                                })}
-                            </TableRow>
-                            </>
-                        )
-                    })}     
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
 
-    
-    </>
+                        <TableRow className='table__row'>
+                            <TableCell>
+
+                            </TableCell>
+                            {questionnairesResponsesToShow.map(collection => {
+
+                                let severity = this.getChipColorFromCategory(collection.category) as string
+
+                                if (collection.status === QuestionnaireResponseStatus.Processed)
+                                    severity = "info"
+
+                                return (
+                                    <TableCell className="answer__table-head" align="center">
+
+                                        <Stack className='answer__header-color' component={Alert} spacing={1} alignItems="center" alignContent="center" alignSelf="center" textAlign="center" icon={false} severity={severity as AlertColor}>
+                                            <div className="answer__header">
+                                                <Typography className="answer__headline" align="center">{collection.answeredTime ? this.datehelper.DayIndexToDay(collection.answeredTime.getUTCDay()) : ""}</Typography>
+                                                <Typography className="answer__date" align="center" variant="caption">{collection.answeredTime ? this.datehelper.DateToString(collection.answeredTime) : ""}</Typography>
+                                            </div>
+
+                                            <ErrorBoundary rerenderChildren={false}>
+                                                <QuestionnaireResponseStatusSelect onUpdate={status => this.statusUpdate(status, collection)} questionnaireResponse={collection} />
+                                            </ErrorBoundary>
+                                        </Stack>
+
+
+
+                                    </TableCell>
+
+                                )
+                            })}
+
+                        </TableRow>
+
+                    </TableHead>
+                    <TableBody>
+                        {this.questionnaireService.findAllQuestions(questionnairesResponsesToShow).map(question => {
+                            return (
+                                <>
+
+                                    <TableRow>
+                                        <TableCell>
+                                            {question.question}
+                                        </TableCell>
+
+                                        {questionnairesResponsesToShow.map(questionResponse => {
+                                            const answer = this.questionnaireService.findAnswer(question, questionResponse);
+                                            const thresholdCollection = this.props.questionnaires.thresholds.find(x => x.questionId == question.Id);
+                                            const category = answer && thresholdCollection ? this.questionAnswerService.FindCategory(thresholdCollection, answer) : CategoryEnum.BLUE
+                                            return (
+                                                <TableCell>
+                                                    <Chip className='answer__chip' component={Box} width="100%" size="medium" color={this.getChipColorFromCategory(category)} label={answer ? answer.ToString() : ""} variant="filled" />
+                                                </TableCell>
+                                            )
+                                        })}
+                                    </TableRow>
+                                </>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+
+        </>
         )
-  }
-    
+    }
+
 }
