f0b2d4662d6fcd971e2aa704f3bfeb1b78a377d3
Mikkel Birch <mib@kvalitetsit.dk>
2022-01-10 12:38:51 +0100

    Handle 404 by not handling it

--- components/Input/SearchPatientInput.tsx
+++ components/Input/SearchPatientInput.tsx
@@ -6,68 +6,76 @@ import { Typography } from '@material-ui/core';
 import { PatientSimple } from '@kvalitetsit/hjemmebehandling/Models/PatientSimple';
 import IPatientService from '../../services/interfaces/IPatientService';
 import { Link } from 'react-router-dom';
+import { NotFoundError } from '@kvalitetsit/hjemmebehandling/Errorhandling/ServiceErrors/NotFoundError';
+import { PatientDetail } from '@kvalitetsit/hjemmebehandling/Models/PatientDetail';
 
 export interface State {
-    patientResults : PatientSimple[]
-    loading : boolean
+  patientResults: PatientSimple[]
+  loading: boolean
 }
 
 
-export class SearchPatientInput extends Component<{},State> {
+export class SearchPatientInput extends Component<{}, State> {
   static displayName = SearchPatientInput.name;
   static contextType = ApiContext
-  patientService! : IPatientService
+  patientService!: IPatientService
 
-  constructor(props : {}){
-      super(props);
-      this.state = {
-          patientResults : [],
-        loading: false
-      }
-      
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      patientResults: [],
+      loading: false
+    }
+
   }
 
-InitialiseServices() : void{
-    this.patientService= this.context.patientService;
-}
+  InitialiseServices(): void {
+    this.patientService = this.context.patientService;
+  }
 
-async searchForPatient(searchString : string) : Promise<void>{
-    this.setState({loading:true})
+  async searchForPatient(searchString: string): Promise<void> {
+    this.setState({ loading: true })
 
-    if(searchString.length < 3){
-        this.setState({patientResults : [], loading : false});
-        return;
+    if (searchString.length < 3) {
+      this.setState({ patientResults: [], loading: false });
+      return;
     }
-        
 
-    try{
-    const patientSearchResults = await this.patientService.SearchPatient(searchString);
-  
-    this.setState({patientResults : patientSearchResults, loading : false});
+    let patientSearchResults: PatientDetail[] = [];
+    try {
+      patientSearchResults = await this.patientService.SearchPatient(searchString);
+
+      this.setState({ patientResults: patientSearchResults, loading: false });
+
+    } catch (error: any) {
+      const noResults = error instanceof NotFoundError
+      if (noResults)
+        patientSearchResults = []
+      else
+        this.setState(() => { throw error })
+    }
+
     this.forceUpdate();
-  }  catch(error : any){
-    this.setState(()=>{throw error})
-  }  
-}
-  render () : JSX.Element{
-      this.InitialiseServices();
+  }
+  render(): JSX.Element {
+    this.InitialiseServices();
     return (
-        <Autocomplete
+      <Autocomplete
         className="search"
         autoComplete
-        getOptionLabel={(option) => option.firstname + " " + option.lastname + " ("+option.cpr+")"}
+        getOptionLabel={(option) => option.firstname + " " + option.lastname + " (" + option.cpr + ")"}
         options={this.state.patientResults}
-        onInputChange={async (a,b)=>await this.searchForPatient(b)}
-        renderOption={(a,b) => (<Stack><Button component={Link} to={"/patients/"+b.cpr}>{b.firstname + " " + b.lastname + " (" + b.cpr + ")"}</Button></Stack>)}
-        renderInput={(params) => <TextField {...params} InputProps={{ 
-            ...params.InputProps,
-            endAdornment: (
-              <InputAdornment position="end" variant="standard">
-                {this.state.loading ? <CircularProgress color="inherit" size={20} /> : ""}
-              </InputAdornment>
-            ),
-          }}  label={<Typography>Søg efter patient</Typography>} />}
-        /> 
+        onInputChange={async (a, b) => await this.searchForPatient(b)}
+        renderOption={(a, b) => (<Stack><Button component={Link} to={"/patients/" + b.cpr}>{b.firstname + " " + b.lastname + " (" + b.cpr + ")"}</Button></Stack>)}
+        renderInput={(params) => <TextField {...params} InputProps={{
+          ...params.InputProps,
+          endAdornment: (
+            <InputAdornment position="end" variant="standard">
+              {this.state.loading ? <CircularProgress color="inherit" size={20} /> : ""}
+            </InputAdornment>
+          ),
+        }} label={<Typography>Søg efter patient</Typography>} />}
+      />
     )
   }
 
