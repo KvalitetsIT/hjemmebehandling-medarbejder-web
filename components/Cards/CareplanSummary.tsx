d4deee1ed161144c35a6d40a1979191d7157a681
Mikkel Birch <mib@kvalitetsit.dk>
2022-01-11 14:18:56 +0100

    Uncloseable dialog after finishing careplan

--- components/Cards/CareplanSummary.tsx
+++ components/Cards/CareplanSummary.tsx
@@ -4,20 +4,23 @@ import CardContent from '@mui/material/CardContent';
 import { Component } from 'react';
 import { PatientCareplan } from '@kvalitetsit/hjemmebehandling/Models/PatientCareplan';
 import Button from '@mui/material/Button';
-import { CardHeader, Divider, Grid, Typography } from '@mui/material';
+import { CardHeader, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, Typography } from '@mui/material';
 import IDateHelper from '@kvalitetsit/hjemmebehandling/Helpers/interfaces/IDateHelper';
 import ApiContext from '../../pages/_context';
 import { Link } from 'react-router-dom';
 import ICareplanService from '../../services/interfaces/ICareplanService';
 import { ConfirmationButton } from '../Input/ConfirmationButton';
 import { PencilIcon } from '../Icons/PencilIcon';
-import { Toast } from '@kvalitetsit/hjemmebehandling/Errorhandling/Toast';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 
 export interface Props {
     careplan: PatientCareplan
 }
 export interface State {
-    toast? : JSX.Element
+    toast?: JSX.Element
+    finishedCareplan: boolean
 }
 
 export class CareplanSummary extends Component<Props, State> {
@@ -25,10 +28,12 @@ export class CareplanSummary extends Component<Props, State> {
     static contextType = ApiContext;
     dateHelper!: IDateHelper
     careplanService!: ICareplanService;
-    
-    constructor(props : Props){
+
+    constructor(props: Props) {
         super(props);
-        this.state = {}
+        this.state = {
+            finishedCareplan: false
+        }
     }
 
     InitialiseServices(): void {
@@ -39,15 +44,47 @@ export class CareplanSummary extends Component<Props, State> {
     async finishCareplan(careplan: PatientCareplan): Promise<void> {
         try {
             await this.careplanService.TerminateCareplan(careplan)
-            const afterResetPasswordToast = (
-                <Toast snackbarTitle="Afslut Monitoreringsplan" snackbarColor="success">
-                    Monitoreringsplanen er nu afsluttet
-                </Toast>
+            const patientName = this.props.careplan.patient?.firstname + " " + this.props.careplan.patient?.lastname
+            const patientCpr = this.props.careplan.patient?.cprToString()
+            const afterResetPasswordToast = this.createNonCloseableDialog(
+                <><CheckCircleOutlinedIcon color='success' /> Monitoreringsplanen er afsluttet</>,
+                "",
+                <>
+                <Typography>Følgende behandlingsplan blev afsluttet</Typography>
+                <Chip icon={<PersonOutlineOutlinedIcon />} label={<Typography padding={2}>{patientName} ({patientCpr})</Typography>} />
+                </>
             )
-            this.setState({toast : afterResetPasswordToast})
+            this.setState({ toast: afterResetPasswordToast })
         } catch (error: any) {
             this.setState(() => { throw error })
         }
+    }
+
+    createNonCloseableDialog(title: string | JSX.Element, subtitle: string | JSX.Element, message: string | JSX.Element): JSX.Element {
+        return (
+            <Dialog fullWidth open={true}>
+                <DialogTitle id="alert-dialog-title">
+                    <Typography variant="subtitle1">{title}</Typography>
+                    <Typography variant="caption">{subtitle}</Typography>
+
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <Typography variant="caption">{message}</Typography>
+                    </DialogContentText>
+
+
+                </DialogContent>
+
+                <DialogActions>
+                    <Button autoFocus onClick={this.reloadPage}><AssignmentIcon />Patientoverblik</Button>
+                </DialogActions>
+            </Dialog>
+        )
+    }
+
+    reloadPage(): void {
+        window.location.replace("/");
     }
 
     render(): JSX.Element {
@@ -55,52 +92,52 @@ export class CareplanSummary extends Component<Props, State> {
         const careplan = this.props.careplan;
         return (
             <>
-            <Card>
-                <CardHeader subheader={
-                    <>
-                        <Grid container alignItems="center">
-                            <Grid item xs={10}>
-                                <Typography variant="h6" fontWeight="bold">Monitoreringsplan</Typography>
+                <Card>
+                    <CardHeader subheader={
+                        <>
+                            <Grid container alignItems="center">
+                                <Grid item xs={10}>
+                                    <Typography variant="h6" fontWeight="bold">Monitoreringsplan</Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Button component={Link} to={"/patients/" + careplan.patient?.cpr + "/edit/plandefinition"}>
+                                        <PencilIcon />
+                                    </Button>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={2}>
-                                <Button component={Link} to={"/patients/" + careplan.patient?.cpr + "/edit/plandefinition"}>
-                                    <PencilIcon />
-                                </Button>
+
+
+                        </>
+                    } />
+                    <Divider />
+                    <CardContent>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Typography variant="caption">
+                                    Patientgrupper
+                                </Typography>
+                                <Typography fontWeight="bold">
+                                    {careplan.planDefinitions.map(x => x.name).join(", ")}
+                                </Typography>
+                                <br />
+                                <Typography variant="caption">
+                                    Opstart
+                                </Typography>
+                                <Typography fontWeight="bold">
+                                    {careplan.creationDate ? this.dateHelper.DateToString(careplan.creationDate) : ""}
+                                </Typography>
                             </Grid>
+                            <Grid item xs={12}>
+                                <ConfirmationButton fullWidth color="error" className='darkColor border' title="Afslut monitoreringsplan?" buttonText="Afslut monitoreringsplan" action={async () => await this.finishCareplan(careplan)}>
+                                    Er du sikker på at du ønsker at afslutte patientens monitoreringsplan?
+                                </ConfirmationButton>
+                            </Grid>
+
                         </Grid>
 
-
-                    </>
-                } />
-                <Divider />
-                <CardContent>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography variant="caption">
-                                Patientgrupper
-                            </Typography>
-                            <Typography fontWeight="bold">
-                                {careplan.planDefinitions.map(x => x.name).join(", ")}
-                            </Typography>
-                            <br />
-                            <Typography variant="caption">
-                                Opstart
-                            </Typography>
-                            <Typography fontWeight="bold">
-                                {careplan.creationDate ? this.dateHelper.DateToString(careplan.creationDate) : ""}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <ConfirmationButton fullWidth color="error" className='darkColor border' title="Afslut monitoreringsplan?" buttonText="Afslut monitoreringsplan" action={async () => await this.finishCareplan(careplan)}>
-                                Er du sikker på at du ønsker at afslutte patientens monitoreringsplan?
-                            </ConfirmationButton>
-                        </Grid>
-
-                    </Grid>
-
-                </CardContent>
-            </Card>
-            {this.state.toast ?? <></>}
+                    </CardContent>
+                </Card>
+                {this.state.toast ?? <></>}
             </>
         );
     }
