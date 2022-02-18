a540b22db3efd789308eb948b5d96ac2f60acce6
Mikkel Birch <mib@kvalitetsit.dk>
2022-02-18 11:57:25 +0100

    Show error with linebreak

--- components/Errors/MissingContactDetailsError.tsx
+++ components/Errors/MissingContactDetailsError.tsx
@@ -3,30 +3,18 @@ import { BaseServiceError, DisplaySettings } from "@kvalitetsit/hjemmebehandling
 export class MissingContactDetailsError extends BaseServiceError {
 
 
-    private missingDetalis: string[]
-  
-    constructor(missingDetalis: string[]) {
-      super();
-      this.missingDetalis = missingDetalis
-    }
-  
-    displayMessage(): string {
-  
-      let message: string = ""
-      this.missingDetalis.forEach(x => {
-        message += x + "\n"
-      })
-      return message;
-    }
-  
-    displayTitle(): string {
-      return this.missingDetalis.length > 0 ? "Følgende informationer mangler" : "Informationer mangler"
-    }
-  
-    displayUrl(): string {
-      return "";
-    }
-    displaySettings(): DisplaySettings {
-      return new DisplaySettings();
-    }
+  private missingDetalis: string[]
+
+  constructor(missingDetalis: string[]) {
+    super();
+    this.missingDetalis = missingDetalis
   }
+
+  displayMessage(): string {
+    return this.missingDetalis.map(x => " - " + x).join("\n")
+  }
+
+  displayTitle(): string {
+    return this.missingDetalis.length > 0 ? "Følgende informationer mangler" : "Informationer mangler"
+  }
+}
