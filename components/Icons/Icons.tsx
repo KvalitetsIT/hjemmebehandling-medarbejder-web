17791f0ab158539b5a2a0a04c11036d6c8360e97
jonas <jqh@kvalitetsit.dk>
2022-03-01 14:30:27 +0100

    added icons to shared

--- components/Icons/Icons.tsx
+++ components/Icons/Icons.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BaseIcon, BaseIconProps } from './Base/BaseIcon';
+import {BaseIcon, BaseIconProps} from "@kvalitetsit/hjemmebehandling/icons/BaseIcon"
 
 
 /* 
