89a71d3834a23881f3b565c3bc98887dc021b4df
orehoj <xxqcj@eg.dk>
2022-08-24 14:31:10 +0200

    removed :-(

--- pages/patients/[cpr]/careplans/[careplanId]/index.tsx
+++ pages/patients/[cpr]/careplans/[careplanId]/index.tsx
@@ -97,7 +97,7 @@ class PatientCareplans extends React.Component<Props, State> {
   renderCareplanTab(): JSX.Element {
     const activeCareplan = this.state.careplans.find(c => c?.id === this.props.match.params.careplanId) ?? this.state.careplans[0]
     return (
-      <IsEmptyCard object={activeCareplan} jsxWhenEmpty="Ingen aktive monitoreringsplaner fundet :-(">
+      <IsEmptyCard object={activeCareplan} jsxWhenEmpty="Ingen aktive monitoreringsplaner fundet">
         <Grid container spacing={3} sx={{ flexWrap: "inherit" }}>
           <Grid maxWidth={400} item xs="auto">
             <ErrorBoundary>
