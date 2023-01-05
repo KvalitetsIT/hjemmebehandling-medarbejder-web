002249de234550eb5ec564b654ece21139ed011d
Sune Koch Hansen <sune@kvalitetsit.dk>
2023-01-05 15:05:19 +0100

    RIM-1017: flyt 'opret patient' knap

--- components/Layout/Topbar.tsx
+++ components/Layout/Topbar.tsx
@@ -1,5 +1,9 @@
 import { Box, Grid } from '@material-ui/core';
+import { Typography, Stack, Button, IconButton, Divider} from '@mui/material';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { SearchPatientInput } from '../Input/SearchPatientInput';
 import { UserContextCard } from '../Cards/UserContextCard';
 import { ErrorBoundary } from '@kvalitetsit/hjemmebehandling/Errorhandling/ErrorBoundary'
@@ -23,6 +27,20 @@ export class Topbar extends Component<{}, State> {
             </ErrorBoundary>
 
 
+          </Grid>
+
+          <Grid item xs={2}>
+            <Stack direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={1}
+              sx={{height: '100%'}}
+            >
+              <Divider orientation='vertical' sx={{backgroundColor: '#5D74AC', pl: 0.25, ml: 3, mr:2, height: '75%'}} />
+              <Button component={Link} to={"/newpatient"} startIcon={<AddCircleRoundedIcon fontSize="large" sx={{ color: '#5D74AC', width:'35px', height: '35px' }} />} >
+                <Typography variant="h6" sx={{ color: '#5D74AC' }}>Opret patient</Typography>
+              </Button>
+            </Stack>
           </Grid>
 
           <Grid item xs>
